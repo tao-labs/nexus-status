179838b609b2a0c408f57225461974acc8f738f4
TAO T-Systems Iberia <tao@t-systems.com>
2016-05-19 19:51:59 +0200

    Update services.js

--- services.js
+++ services.js
@@ -244,10 +244,10 @@ function parseHero(hero, ID){
 							parser: false,
 							
 							// string - By default, unit will automatically be detected.  Override with 'week', 'month', 'year', etc. (see supported time measurements)
-							unit: 'minute',
+							unit: 'hour',
 
 							// Number - The number of steps of the above unit between ticks
-							unitStepSize: 60,
+							unitStepSize: 2,
 
 							// string - By default, no rounding is applied.  To round, set to a supported time unit eg. 'week', 'month', 'year', etc.
 							//round: 'hour',
