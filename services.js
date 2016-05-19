858080bf2efe52f88a4c4dce0461a38620d887a0
TAO T-Systems Iberia <tao@t-systems.com>
2016-05-19 21:43:47 +0200

    Update services.js

--- services.js
+++ services.js
@@ -247,7 +247,7 @@ function parseHero(hero, ID){
 							unit: 'hour',
 
 							// Number - The number of steps of the above unit between ticks
-							unitStepSize: 2,
+							unitStepSize: 3,
 
 							// string - By default, no rounding is applied.  To round, set to a supported time unit eg. 'week', 'month', 'year', etc.
 							//round: 'hour',
