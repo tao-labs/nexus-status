bf6a832567680f4dd527f8060bba9c54aa861330
TAO T-Systems Iberia <tao@t-systems.com>
2016-06-06 20:18:27 +0200

    Update issues.js

--- issues.js
+++ issues.js
@@ -1,1 +1,9 @@
-{}
+var issues = [
+		{
+			subject: "Labs no se encuentra disponible",
+			description: "El servidor de preproducción se encuentra pendiente de actualización de la base de datos",
+			type: "info_outline", //info_outline, warning, error
+			start: "2016-06-06 17:00",
+			end: "2016-06-07 13:00",
+		},
+];
